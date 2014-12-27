f8cd66d7b839fc50a31faa8bac620c98db39ee5f
welefen <welefen@gmail.com>
2014-12-27 12:30:43 +0800

    update

--- test/index.js
+++ test/index.js
@@ -2,4 +2,4 @@ var str = require('fs').readFileSync('1.txt', 'utf8');
 var flkit = require('../lib/index.js');
 var instance = flkit.getInstance('html', 'lexer', str);
 var result = instance.run();
-console.log(result)
+console.log(JSON.stringify(result, undefined, 4))
