f9a36cd50a55e7c4b84a2dd03fbccd1371e208e9
welefen <welefen@gmail.com>
2014-12-30 10:31:56 +0800

    update

--- test/index.js
+++ test/index.js
@@ -1,7 +1,41 @@
-var str = require('fs').readFileSync('1.txt', 'utf8');
 var flkit = require('../lib/index.js');
-var instance = flkit.getInstance('html', 'lexer', str);
-var start = Date.now();
-var result = instance.run();
-var end = Date.now();
-console.log(end - start)
+var casePath = __dirname + '/case/';
+var fs = require('fs');
+var assert = require('assert');
+
+var think = require('thinkjs-util');
+
+['html', 'css', 'js'].forEach(function(lang){
+  if (!think.isDir(casePath + lang)) {
+    return;
+  }
+  describe(lang, function(){
+    var files = fs.readdirSync(casePath + lang);
+    files.forEach(function(file){
+      describe(file.split('.')[0], function(){
+        var filePath = casePath + lang + '/' + file;
+        var data = JSON.parse(think.getFileContent(filePath) || '{}');
+        for(var key in data){
+          it(key, function(){
+            var instance = flkit.getInstance(lang, file.split('.')[0], data[key].code);
+            instance.tpl = data[key].tpl;
+            instance.ld = (data[key].ld || '').split(',');
+            instance.rd = (data[key].rd || '').split(',');
+            var ret;
+            try{
+              ret = instance.run(data[key].options);
+            }catch(e){
+              ret = e.toString();
+            }
+            if (typeof ret === 'string') {
+              assert.equal(ret, data[key].result);
+            }else{
+              assert.equal(JSON.stringify(ret), JSON.stringify(data[key].result));
+            }
+          })
+        }
+      })
+    })
+  })
+  
+})
--- test/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once '../../stc/src/vender/Fl/src/Fl.class.php';
-Fl::loadClass('Fl_Html_Token');
-$content = file_get_contents('1.txt');
-$instance = new Fl_Html_Token($content);
-$start = microtime(true);
-$instance->run();
-$end = microtime(true);
-echo ($end - $start) * 1000;
