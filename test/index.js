ea55dcdf988f982ddd7fd4a8445114626ecc3894
welefen <welefen@gmail.com>
2014-12-30 12:00:51 +0800

    update html lexer test case

--- test/index.js
+++ test/index.js
@@ -6,6 +6,7 @@ var muk = require('muk');
 
 var think = require('thinkjs-util');
 var flkit = require('../lib/index.js');
+var cls = flkit.load('html', 'lexer');
 
 ['html', 'css', 'js'].forEach(function(lang){
   if (!think.isDir(casePath + lang)) {
@@ -18,23 +19,33 @@ var flkit = require('../lib/index.js');
         var filePath = casePath + lang + '/' + file;
         var data = JSON.parse(think.getFileContent(filePath) || '{}');
         for(var key in data){
-          it(key, function(){
-            var instance = flkit.getInstance(lang, file.split('.')[0], data[key].code);
-            instance.tpl = data[key].tpl;
-            instance.ld = (data[key].ld || '').split(',');
-            instance.rd = (data[key].rd || '').split(',');
-            var ret;
-            try{
-              ret = instance.run(data[key].options);
-            }catch(e){
-              ret = e.toString();
+          it(key, (function(key){
+            return function(){
+              var instance = flkit.getInstance(lang, file.split('.')[0], data[key].code);
+              instance.tpl = data[key].tpl;
+              if (data[key].ld) {
+                instance.ld = data[key].ld.split(',');
+              }else{
+                instance.ld = '';
+              }
+              if (data[key].rd) {
+                instance.rd = data[key].rd.split(',');
+              }else{
+                instance.rd = '';
+              }
+              var ret;
+              try{
+                ret = instance.run(data[key].options);
+              }catch(e){
+                ret = e.toString();
+              }
+              if (typeof ret === 'string') {
+                assert.equal(ret, data[key].result);
+              }else{
+                assert.equal(JSON.stringify(ret), JSON.stringify(data[key].result));
+              }
             }
-            if (typeof ret === 'string') {
-              assert.equal(ret, data[key].result);
-            }else{
-              assert.equal(JSON.stringify(ret), JSON.stringify(data[key].result));
-            }
-          })
+          })(key))
         }
       })
     })
