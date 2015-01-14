fa08c2a786adc902c43867fe700754be89b260ee
welefen <welefen@gmail.com>
2015-01-14 13:00:56 +0800

    update css lexer case

--- benchmark/css_lexer/index.js
+++ benchmark/css_lexer/index.js
@@ -1,10 +1,19 @@
+// var agent = require('webkit-devtools-agent');
+// agent.start({
+//   port: 9999,
+//   bind_to: '0.0.0.0',
+//   ipc_port: 3333,
+//   verbose: true
+// });
 var content = require('fs').readFileSync('page.css', 'utf8');
 var flkit = require('../../index.js');
-var startTime = Date.now();
 var lexerInstance = flkit.getInstance('css', 'lexer', content);
-var ret = lexerInstance.run({
- 
-});
-var endTime = Date.now();
-console.log(endTime - startTime);
-require('fs').writeFileSync('result.json', JSON.stringify(ret, undefined, 4))
+// setTimeout(function(){
+  var startTime = Date.now();
+  var ret = lexerInstance.run({});
+  var endTime = Date.now();
+  console.log(endTime - startTime);
+// }, 20000)
+
+//
+//require('fs').writeFileSync('result.json', JSON.stringify(ret, undefined, 4))
